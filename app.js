4821e1c769921498eeaf26c098dc7fa365a19714
Priyanshi Jain <91975234+priyanshij16@users.noreply.github.com>
2023-02-10 17:22:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// for making our search bar works 
+const search =()=>{
+    // value enter by user will get stored by searchbox
+  const searchbox = document.getElementById("search-item").value.toUpperCase();
+  const storeitems = document.getElementById("product-list")
+  const product= document.querySelectorAll(".product")
+  const pname = document.getElementsByTagName("h2")
+
+   for(var i=0; i<pname.length; i++)
+   {
+    let match =product[i].getElementsByTagName('h2')[0];
+    if(match){
+         let text= match.textContent || match.innerHTML
+         if(text.toUpperCase(). indexOf(searchbox) >-1){
+            product[i].style.display ="";
+         }
+         else
+         {
+             product[i].style.display = "none";  
+         }
+    }
+   }
+ } 
